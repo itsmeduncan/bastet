0e2c50f66e7e042fdd5530467153b29f6e924f51
Duncan Grazier <itsmeduncan@gmail.com>
2011-12-07 15:21:28 -0500

    Add test coverage for complex criteria in groups as well as the default behavior

--- spec/bastet/base_spec.rb
+++ spec/bastet/base_spec.rb
@@ -13,6 +13,23 @@ describe Bastet::Base do
       @bastet.activate(:banana, group)
       @bastet.active?(:banana, user).should be_true
     end
+
+    describe "complex criteria" do
+      before do
+        @group = Bastet::Group.new("20_percent") { |entity| (entity.id % 10) < (20 / 10) }
+        @bastet.activate(:secret_feature, @group)
+      end
+
+      it "should be true for the user" do
+        user = mock('user', id: 20)
+        @bastet.active?(:secret_feature, user).should be_true
+      end
+
+      it "should be false for the user" do
+        user = mock('user', id: 19)
+        @bastet.active?(:secret_feature, user).should be_false
+      end
+    end
   end
 
   describe "deactivate" do
@@ -25,6 +42,13 @@ describe Bastet::Base do
 
       @bastet.deactivate(:banana, group)
       @bastet.inactive?(:banana, user).should be_true
+    end
+  end
+
+  describe "default to inactive" do
+    it "should default to inactive for new features" do
+      user = mock('user')
+      @bastet.active?(:unknown_feature, user).should be_false
     end
   end
 end
